fd2ed8689927f81484b44d1f8d5b75ef0b3a9b75
Mechiel Lukkien <m.lukkien@irias.nl>
2018-01-17 15:50:14 +0100

    update release.sh with latest standards

--- release.sh
+++ release.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-mkdir local 2>/dev/null
-
-exec env NAME=$(basename $PWD) \
+exec env GOOS=$GOOS GOARCH=$GOARCH \
+NAME=$(basename $PWD) \
 VERSION=$(git describe --tags | sed 's/^v//') \
 GOVERSION=$(go version | cut -f3 -d' ') \
 sh -c '
-	CGO_ENABLED=0;
-	mkdir local/${NAME}-${VERSION:-x} 2>/dev/null;
+	set -e
+	export CGO_ENABLED=0
 	SUFFIX=""
 	if test $GOOS = windows; then
 		SUFFIX=.exe
 	fi
-	DEST=local/${NAME}-${VERSION:-x}/${NAME}-${VERSION:-x}-${GOOS:-x}-${GOARCH:-x}-${GOVERSION:-x}-${BUILDID:-0}${SUFFIX};
-	go build -ldflags "-X main.version=${VERSION:-x}" &&
-	mv ${NAME}${SUFFIX} $DEST &&
+	DEST=local/${NAME}-${VERSION:-x}-${GOOS:-x}-${GOARCH:-x}-${GOVERSION:-x}-${BUILDID:-0}${SUFFIX}
+	go build -ldflags "-X main.version=${VERSION:-x}"
+	mv $NAME${SUFFIX} $DEST
 	echo release: $NAME $VERSION $GOOS $GOARCH $GOVERSION $DEST
 '
